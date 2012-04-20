e92af25cc2773a42e5934752e0db2055e72e0ccc
Jun Sugahara <j.sghr.jp@gmail.com>
2012-04-20 10:09:06 +0900

    add microsec field, using LOAD DATA LOCAL INFILE

--- dump_analyser/tcpdump2db.rb
+++ dump_analyser/tcpdump2db.rb
@@ -2,47 +2,17 @@
 require 'pcap'
 require 'mysql'
 
-#print 'Host:'
-#host = STDIN.gets
-#print 'id:'
-#id = STDIN.gets
-#print 'pass:'
-#system "stty -echo"
-#pass = $stdin.gets.chop
-#system "stty echo"
 
-# Mysql init set
-@db = Mysql::new(ARGV[1],"ruby","suga0329","tcpdump")
-
-
-@table_name = File::basename(ARGV[0])
-@filename = ARGV[0]
-@dirname = File.dirname(ARGV[0])
-
-# create table
-sql = "CREATE TABLE `tcpdump`.`#{@table_name}` (`number` INT NOT NULL DEFAULT NULL AUTO_INCREMENT PRIMARY KEY ,`time` DATETIME NOT NULL ,`protocol_1` TEXT DEFAULT NULL ,`protocol_2` TEXT DEFAULT NULL ,`protocol_3` TEXT DEFAULT NULL ,`protocol_4` TEXT DEFAULT NULL ,`eth_src` TEXT DEFAULT NULL ,`eth_dst` TEXT DEFAULT NULL , `ip_src` INT UNSIGNED DEFAULT NULL ,`ip_dst` INT UNSIGNED DEFAULT NULL ,`tcp_srcport` INT DEFAULT NULL ,`tcp_dstport` INT DEFAULT NULL ,`udp_srcport` INT DEFAULT NULL,`udp_dstport` INT DEFAULT NULL, `length` INT DEFAULT NULL) ENGINE = MYISAM"
-@db.query(sql)
-
-# run tshark from file and change output fields, store output to result
-#`tshark -r #{@filename} -T fields -e frame.time -e frame.protocols -e eth.src -e eth.dst -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e udp.srcport -e udp.dstport -e frame.len -E separator=\\; > #{@dirname}/temp.txt;`
-
-file = File.open("#{@dirname}/temp.txt", 'r')
-
-# get header info from lines
-line_cnt = 1
-sql = "INSERT INTO `#{@table_name}` VALUES"
-start_time=Time.new
-
-while line = file.gets
-  line_cnt += 1
+def get_header(line)
   line = line.chomp
   frame_prop = line.split(";")
-  month = frame_prop[0].split(",")[0].split(" ")[0]#後回し
+  month = frame_prop[0].split(",")[0].split(" ")[0]
   day = frame_prop[0].split(",")[0].split(" ")[1]
   year = frame_prop[0].split(",")[1].split(" ")[0]
   hour = frame_prop[0].split(",")[1].split(" ")[1].split(":")[0]
   minute = frame_prop[0].split(",")[1].split(" ")[1].split(":")[1]
   second =  frame_prop[0].split(",")[1].split(" ")[1].split(":")[2].split(".")[0]
+  micro_second = frame_prop[0].split(",")[1].split(" ")[1].split(":")[2].split(".")[1][0..5]
   frame_time = Time.local(year, month, day, hour, minute, second)
   frame_protocols = frame_prop[1].split(":")
   eth_src = frame_prop[2]
@@ -54,8 +24,9 @@ while line = file.gets
   udp_srcport = frame_prop[8]
   udp_dstport = frame_prop[9]
   length = frame_prop[10]
-  sql_insert = {
-    "time" => frame_time.strftime("%Y%m%d%H%M%S"),
+  header = {
+    "time" => frame_time.strftime("%Y-%m-%d %H:%M:%S"),
+    "micro_second" => micro_second,
     "protocol_1" => frame_protocols[0],
     "protocol_2" => frame_protocols[1],
     "protocol_3" => frame_protocols[2],
@@ -70,67 +41,46 @@ while line = file.gets
     "udp_dstport" => frame_prop[9],
     "length" => length
   }
-  
-  
-  sql += "(NULL, "
-  value_cnt = 0
-  sql_insert.each do |key, value|
+  header
+end
+
+@db = Mysql::init()
+@db.options(Mysql::OPT_LOCAL_INFILE)
+@db.real_connect(ARGV[1],"ruby","suga0329","tcpdump")
+
+@table_name = File::basename(ARGV[0])
+@filename = ARGV[0]
+@dirname = File.dirname(ARGV[0])
+
+# create table
+sql = "CREATE TABLE `tcpdump`.`#{@table_name}` (`number` INT NOT NULL DEFAULT NULL AUTO_INCREMENT PRIMARY KEY ,`time` DATETIME NOT NULL ,`micro_second` INT NOT NULL,`protocol_1` TEXT DEFAULT NULL ,`protocol_2` TEXT DEFAULT NULL ,`protocol_3` TEXT DEFAULT NULL ,`protocol_4` TEXT DEFAULT NULL ,`eth_src` TEXT DEFAULT NULL ,`eth_dst` TEXT DEFAULT NULL , `ip_src` TEXT DEFAULT NULL ,`ip_dst` TEXT DEFAULT NULL ,`tcp_srcport` INT DEFAULT NULL ,`tcp_dstport` INT DEFAULT NULL ,`udp_srcport` INT DEFAULT NULL,`udp_dstport` INT DEFAULT NULL, `length` INT DEFAULT NULL) ENGINE = MYISAM"
+#@db.query(sql)
+
+# run tshark from file and change output fields, store output to result
+`tshark -r #{@filename} -T fields -e frame.time -e frame.protocols -e eth.src -e eth.dst -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e udp.srcport -e udp.dstport -e frame.len -E separator=\\; > #{@dirname}/temp.txt;`
+# result = `tshark -r #{@filename} -T fields -e frame.time -e frame.protocols -e eth.src -e eth.dst -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e udp.srcport -e udp.dstport -e frame.len -E separator=\\;`
+
+file = File.open("#{@dirname}/temp.txt", 'r')
+
+# get header info from lines
+start_time=Time.new
+file_csv = File.open("#{@dirname}/data.csv",'w')
+while line = file.gets
+  header = get_header(line)
+  csv_line = 'NULL,'
+  header.each do |key,value|
     if value != "" && value != nil
-      sql += ", " if value_cnt != 0
-      if key.index("ip_")
-        sql += "INET_ATON(\"#{value}\")"
-        value_cnt += 1
-      else
-        sql += "'#{value}'"
-        value_cnt += 1
-      end
-    else 
-      sql += ", " if value_cnt != 0
-      sql += "NULL"
-      value_cnt += 1
+      csv_line+="\"#{value}\","
+    else
+      csv_line+='NULL,'
     end
   end
-  sql += ")"
-  #p sql
-  if line_cnt > 100
-    #p sql
-    @db.query(sql)
-    sql = "INSERT INTO `#{@table_name}` VALUES"
-    line_cnt = 1
-  else
-    sql += ","
-  end
-end
-if sql[sql.size-1] == ","
-  sql = sql.chop 
-  @db.query(sql)
+  csv_line = csv_line.chop
+  file_csv.puts csv_line
 end
 
-end_time = Time.new
+file_csv.close
 
-p end_time - start_time
 
-#cap = Pcap::Capture.open_offline(ARGV[0])
-
-#cap.loop do |pkt|
-#  if pkt.tcp?
-#    time = pkt.time.strftime("%Y%m%d%H%M%S")
-#    src_ip = pkt.ip_src.to_i
-#    dst_ip = pkt.ip_dst.to_i
-#    src_port = pkt.sport
-#    dst_port = pkt.dport
-#    length = pkt.ip_len
-#    sql = "INSERT INTO `#{@table_name}` (`number` ,`time` ,`protocol` ,`source_ip` ,`destination_ip` ,`source_port` ,`destination_port` ,`length`)VALUES (NULL , '#{time}', 'tcp','#{src_ip}', '#{dst_ip}', '#{src_port}', '#{dst_port}', '#{length}')"
-#  elsif pkt.udp?
-#    time = pkt.time.strftime("%Y%m%d%H%M%S")
-#    src_ip = pkt.ip_src.to_i
-#    dst_ip = pkt.ip_dst.to_i
-#    src_port = pkt.sport
-#    dst_port = pkt.dport
-#    length = pkt.ip_len
-#    sql = "INSERT INTO `#{@table_name}` (`number` ,`time` ,`protocol` ,`source_ip` ,`destination_ip` ,`source_port` ,`destination_port` ,`length`)VALUES (NULL , '#{time}', 'udp','#{src_ip}', '#{dst_ip}', '#{src_port}', '#{dst_port}', '#{length}')"
-#  else
-#    next
-#  end
-#  @db.query(sql)
-#end
+sql = "LOAD DATA LOCAL INFILE '#{@dirname}/data.csv' INTO TABLE `#{@table_name}` FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\\n'"
+@db.query(sql)
